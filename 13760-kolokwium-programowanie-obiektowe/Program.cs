25f74bb254114da706f910f7ee290312f75aee3f
Bartlomiej.Nalepa <bnalepa@o2.pl>
2022-06-15 18:01:48 +0200

    Dodaj pliki projektów.

---
+++ 13760-kolokwium-programowanie-obiektowe/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace _13760_kolokwium_programowanie_obiektowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            var d1 = new Delegat(Potega);
+            d1 += new Delegat(Potega2);
+            d1(2, 3);
+            // 2 
+            double[] tab = new double[10000];
+
+            Stopwatch s1 = Stopwatch.StartNew();
+
+            foreach (var i in tab)
+            {
+                Console.Write(i+10+", ");
+            }
+            s1.Stop();
+            Stopwatch s2 = Stopwatch.StartNew();
+            Parallel.ForEach(tab, i =>
+            {
+                Console.Write(i + 10 + ", ");
+            });
+            s2.Stop();
+            Console.WriteLine("program 1 - "+s1.Elapsed);
+            Console.WriteLine("program 2 - "+s2.Elapsed);
+
+        }
+        // 1 
+        static public void Potega(int x, int y)
+        {
+            Console.WriteLine(Math.Pow(x, y));
+        }
+        public delegate void Delegat(int x, int y);
+        static public void Potega2(int x, int y)
+        {
+            Console.WriteLine(Math.Pow(y, x));
+        }
+        
+
+
+    }
+}
---
+++ kolos - zad 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace kolos___zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metoda<int>(6, 5);
+            Metoda<int>(4, 5);
+            Metoda<int>(5, 5);
+        }
+
+
+        static public void Metoda<T>(T x1, T x2) where T: IComparable
+        {
+            switch(x1.CompareTo(x2))
+            {
+                case < 0:
+                    Console.WriteLine(x2);
+                    break;
+                case > 0:
+                    Console.WriteLine(x1);
+                    break;
+                 default:
+                    Console.WriteLine("Są równe sobie");
+                    break;
+
+            }
+        }
+    }
+}
